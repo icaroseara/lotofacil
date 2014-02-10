d110a958efd2068a1f8402628032abc8469fcbc2
=Icaro <ikro89@gmail.com>
2014-02-10 13:54:36 -0300

    generate fixed dozens

--- lib/lottery.rb
+++ lib/lottery.rb
@@ -1,12 +1,21 @@
 class Lottery
-	attr_reader :previous_results
+	attr_reader :previous_results, :fixed_dozens
 	def initialize
 		@previous_results = []
+		@fixed_dozens = []
 	end
-	def add_previous_result previous_result
+	def add_previous_result *previous_result
 		@previous_results << previous_result.to_a if previous_result.size == 15
 	end
 	def find_repeated_dozens
 		@previous_results.first & @previous_results.last if @previous_results.size == 2
 	end
+	def generate_fixed_dozens		
+		@fixed_dozens = find_repeated_dozens.sort 
+		while @fixed_dozens.size != 14			
+			item = @previous_results.last.sample
+			@fixed_dozens.push(item) unless fixed_dozens.include?(item)			
+		end	
+		@fixed_dozens.sort!
+	end	
 end
--- spec/lottery_spec.rb
+++ spec/lottery_spec.rb
@@ -6,20 +6,24 @@ describe Lottery do
 	context "when the lottery adds the previous result" do
 		before do 
 			@lottery = Lottery.new
-			@lottery.add_previous_result(1..15)
+			@lottery.add_previous_result(1,2,3,5,6,7,8,13,15,16,17,18,20,21,23)
 		end
 		it "is possible add previous lottery result" do 				
 			expect(@lottery.previous_results.last.size).to eq(15)
 		end
 		context "when the lottery adds the two previous result" do
 			before do
-				@lottery.add_previous_result(11..25)
+				@lottery.add_previous_result(1,2,3,5,6,11,12,14,15,16,19,20,21,23,25)
 			end			
 			it "is possible add two previous lottery's results" do 				
 				expect(@lottery.previous_results.size).to eq(2)
 			end
 			it "is possible find dozens repeated in two previous results" do
-				expect(@lottery.find_repeated_dozens.size).to eq(5)
+				expect(@lottery.find_repeated_dozens.size).to eq(10)
+			end
+			it "its possible generate fixed_dozens" do
+				@lottery.generate_fixed_dozens
+				expect(@lottery.fixed_dozens.size).to eq(14)
 			end
 		end
 	end
