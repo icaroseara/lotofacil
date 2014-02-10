b4c55e2ce2b08a4a9ce91b3e3f99d729167fe345
=Icaro <ikro89@gmail.com>
2014-02-10 13:05:40 -0300

    find dozens repeated in previous lottery's results

--- lib/lottery.rb
+++ lib/lottery.rb
@@ -4,6 +4,9 @@ class Lottery
 		@previous_results = []
 	end
 	def add_previous_result previous_result
-		@previous_results << previous_result.to_a
+		@previous_results << previous_result.to_a if previous_result.size == 15
+	end
+	def find_repeated_dozens
+		@previous_results.first & @previous_results.last if @previous_results.size == 2
 	end
 end
--- spec/lottery_spec.rb
+++ spec/lottery_spec.rb
@@ -3,16 +3,24 @@ require "spec_helper"
 require "lottery"
 
 describe Lottery do 
-	before do 
-		@lottery = Lottery.new
-	end
-	it "is possible add previous lottery result" do 				
-		@lottery.add_previous_result(1..15)
-		expect(@lottery.previous_results.last.size).to eq(15)
-	end
-	it "is possible add two previous lottery's results" do 
-		@lottery.add_previous_result(1..15)
-		@lottery.add_previous_result(1..15)
-		expect(@lottery.previous_results.size).to eq(2)
+	context "when the lottery adds the previous result" do
+		before do 
+			@lottery = Lottery.new
+			@lottery.add_previous_result(1..15)
+		end
+		it "is possible add previous lottery result" do 				
+			expect(@lottery.previous_results.last.size).to eq(15)
+		end
+		context "when the lottery adds the two previous result" do
+			before do
+				@lottery.add_previous_result(11..25)
+			end			
+			it "is possible add two previous lottery's results" do 				
+				expect(@lottery.previous_results.size).to eq(2)
+			end
+			it "is possible find dozens repeated in two previous results" do
+				expect(@lottery.find_repeated_dozens.size).to eq(5)
+			end
+		end
 	end
 end
