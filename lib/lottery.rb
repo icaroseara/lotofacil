186e37ebf69f019ff8ccf846ee07349c731b26e6
=Icaro <ikro89@gmail.com>
2014-02-11 10:27:59 -0300

    completing fixed dozens with numbers that not appered in previous results

--- lib/lottery.rb
+++ lib/lottery.rb
@@ -17,16 +17,13 @@ class Lottery
 		@previous_results.first & @previous_results.last if @previous_results.size == 2
 	end
 	def generate_fixed_dozens		
-		@fixed_dozens = find_repeated_dozens.sort 
-		while @fixed_dozens.size != 14			
-			item = @previous_results.last.sample
-			@fixed_dozens.push(item) unless fixed_dozens.include?(item)			
-		end	
+		@fixed_dozens = find_repeated_dozens.sort 		
+		possibilities = (1..25).to_a - @previous_results.last		
+		@fixed_dozens = @fixed_dozens | possibilities.sample(14 - @fixed_dozens.size)		
 		@fixed_dozens.sort!
 	end	
-	def generate_remainding_dozens
-		all = 1..25
-		@remainding_dozens = all.to_a.select{|item| !fixed_dozens.include?(item)}
+	def generate_remainding_dozens		
+		@remainding_dozens = (1..25).to_a.select{|item| !fixed_dozens.include?(item)}
 	end
 	def generate_bets		
 		@remainding_dozens.each do |r|
