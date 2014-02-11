594f220e06a76896b730c83a8a1eca405cf2d124
=Icaro <ikro89@gmail.com>
2014-02-11 09:20:10 -0300

    Changes in result's exibition

--- bin/lotofacil.rb
+++ bin/lotofacil.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/lottery'
 
 loto = Lottery.new
-# add here the first previous result
+# change the next line with the first previous result
 loto.add_previous_result(1,2,3,5,6,11,12,14,15,16,19,20,21,23,25)
-# add here the second previous result
+# change the next line with the second previous result
 loto.add_previous_result(1,2,3,4,8,9,10,11,12,15,16,17,19,21,23)
 repeated_dozens= loto.find_repeated_dozens
 puts "Repeated dozens #{repeated_dozens.size.to_s}"
